9df3731cede221e95dd7646e7e87d75406dd90c8
rizwan-zafar <rizwan.zafar@systemsltd.com>
2025-04-18 14:46:25 +0500

    Footer added in XF

--- ui.frontend/src/ewallet/Ewallet.js
+++ ui.frontend/src/ewallet/Ewallet.js
@@ -1,11 +1,14 @@
 // Ewallet.js
 import React from 'react';
 import Header from './js/Header/Header';
+import Footer from './js/Footer/Footer';
 
 const Ewallet = () => {
   return (
     <div>
       <Header />
+      <h1>testing</h1>
+      <Footer />
     </div>
   );
 };
